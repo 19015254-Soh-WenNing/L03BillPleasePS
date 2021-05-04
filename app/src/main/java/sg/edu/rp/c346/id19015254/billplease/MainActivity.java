ac2203e6ade146a2e5b84b8353774e4d7ee2236c
19015254 <19015254@myrp.edu.sg>
2021-05-04 16:59:15 +0800

    New changes

--- app/src/main/java/sg/edu/rp/c346/id19015254/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19015254/billplease/MainActivity.java
@@ -48,27 +48,35 @@ public class MainActivity extends AppCompatActivity {
                 String pax = paxET.getText().toString();
                 String disc = discET.getText().toString();
 
-                Double amt1 = Double.parseDouble(amtET.getText().toString());
-                Integer pax1 = Integer.parseInt(paxET.getText().toString());
-                Integer disc1 = Integer.parseInt(discET.getText().toString());
+                Double amt1 = 0.0;
+                Integer pax1 = 0;
+                Integer disc1 = 0;
 
                 Double total = 0.0;
                 Double each = 0.0;
                 int checked = rg.getCheckedRadioButtonId();
 
                 if (amt.isEmpty() == true || pax.isEmpty() == true || disc.isEmpty() == true) {
-                    Toast.makeText(MainActivity.this, "Please input all fields", Toast.LENGTH_SHORT).show();
-                } else {
+                    Toast.makeText(MainActivity.this, "Please input all fields", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    amt1 = Double.parseDouble(amtET.getText().toString());
+                    pax1 = Integer.parseInt(paxET.getText().toString());
+                    disc1 = Integer.parseInt(discET.getText().toString());
+
                     if (amt1 < 0) {
                         totalTV.setText("Please enter appropriate amount!");
                         totalTV.setTextColor(Color.RED);
-                    } else if (pax1 < 0) {
+                    }
+                    else if (pax1 < 0) {
                         totalTV.setText("Please enter appropriate number of pax!");
                         totalTV.setTextColor(Color.RED);
-                    } else if (disc1 < 0) {
+                    }
+                    else if (disc1 < 0) {
                         totalTV.setText("Please enter appropriate discount!");
                         totalTV.setTextColor(Color.RED);
-                    } else {
+                    }
+                    else {
                         if (gstTB.isChecked() == true && svsTB.isChecked() == true) {
                             amt1 = amt1 - (amt1 * (disc1 * 0.01));
                             total = amt1 + (amt1 * 0.1) + (amt1 * 0.07);
@@ -85,19 +93,19 @@ public class MainActivity extends AppCompatActivity {
                             total = amt1 - (amt1 * (disc1 * 0.01));
                             each = total / pax1;
                         }
-                        else {
-                            if (checked == R.id.cashRB) {
-                                totalTV.setText("Total Bill: $" + total.toString());
-                                totalTV.setText("Each Pays: $" + each.toString() + " in cash");
-                            } else if (checked == R.id.pnRB) {
-                                totalTV.setText("Total Bill: $" + total.toString());
-                                totalTV.setText("Each Pays: $" + each.toString() + " via PayNow to 912345678");
-                            } else {
-                                totalTV.setText("Please choose payment method first!");
-                                totalTV.setTextColor(Color.RED);
-                            }
-                        }
                     }
+                }
+                if (checked == R.id.cashRB) {
+                    totalTV.setText("Total Bill: $" + String.format("%.2f", total));
+                    eachTV.setText("Each Pays: $" + String.format("%.2f", each) + " in cash");
+                    eachTV.setTextColor(Color.BLACK);
+                } else if (checked == R.id.pnRB) {
+                    totalTV.setText("Total Bill: $" + String.format("%.2f", total));
+                    eachTV.setText("Each Pays: $" + String.format("%.2f", each) + " via PayNow to 912345678");
+                    eachTV.setTextColor(Color.BLACK);
+                } else {
+                    eachTV.setTextColor(Color.RED);
+                    totalTV.setText("Please choose payment method first!");
                 }
             }
         });
@@ -107,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 totalTV.setText("");
                 eachTV.setText("");
+                eachTV.setTextColor(Color.BLACK);
                 amtET.setText("");
                 paxET.setText("");
                 discET.setText("");
